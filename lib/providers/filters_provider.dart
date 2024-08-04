de616310c0fe66a8995d58753b7a39bd239fe966
ali.aljalo <admirrlaty#gmail.com>
2024-08-04 23:54:00 +0300

    Connected multiple providers with wach other Dependent Providers

--- lib/providers/filters_provider.dart
+++ lib/providers/filters_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/providers/meals_provider.dart';
 
 enum Filter {
   glutenFree,
@@ -32,3 +33,24 @@ final filtersProvider =
     StateNotifierProvider<FiltersNoifier, Map<Filter, bool>>(
   (ref) => FiltersNoifier(),
 );
+
+final filteredMealProvider = Provider((ref) {
+  final meals = ref.watch(mealsProvider);
+  final activeFilters = ref.watch(filtersProvider);
+
+  return meals.where((meal) {
+    if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+      return false;
+    }
+    if (activeFilters[Filter.lactosefree]! && !meal.isLactoseFree) {
+      return false;
+    }
+    if (activeFilters[Filter.vegan]! && !meal.isVegan) {
+      return false;
+    }
+    if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+      return false;
+    }
+    return true;
+  }).toList();
+});
